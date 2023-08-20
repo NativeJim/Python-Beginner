05b177dfff80a7997a7270d26e0b4ce62a8f8c51
Jereme Sorteberg <jeremesorteberg059@gmail.com>
2023-08-19 23:33:33 -0500

    Day 1

---
+++ day-1.py
@@ -0,0 +1,9 @@
+bool_one = 2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
+
+bool_two = 3 < 4 or 'Arthur' == 'Arturo'
+
+bool_three = 100 ** 0.5 >= 50 or -- 4 == -4
+
+bool_four = 4==4 or 4==4
+
+bool_five = 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
